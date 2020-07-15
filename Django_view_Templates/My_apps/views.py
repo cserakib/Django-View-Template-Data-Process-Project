a1d93d6436370e8bb1d738b1c13e817dc6273c3b
rakib <cserakib13@gmail.com>
2020-07-15 22:19:20 +0600

    Here add a context function for django data procss

--- Django_view_Templates/My_apps/views.py
+++ Django_view_Templates/My_apps/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwagrs):
+        context = super().get_context_data(** kwagrs)
+        
+        context['name'] = "Rakibul Islam" 
+        context['country'] = "Bangladesh"
+
+        list = [1,2,3,4,5,6]
+        context['list'] = list 
+
+
+        return context
